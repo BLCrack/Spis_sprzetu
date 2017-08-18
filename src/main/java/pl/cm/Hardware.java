ba23963fb28dbecc39c67cddcb7c008727e58cc8
BLCrack <quaresmaaa10@wp.pl>
2017-08-18 12:26:04 +0200

    Zmiana nazw klas

---
+++ src/main/java/pl/cm/Hardware.java
@@ -0,0 +1,68 @@
+package pl.cm;
+
+public class Hardware
+{
+    private int inventoryNumber;
+    private String serialNumber;
+    private String name;
+    private String localization;
+    private String previousOwner;
+    private String accessory;
+    private String information;
+
+    public int getInventoryNumber() {
+        return inventoryNumber;
+    }
+
+    public void setInventoryNumber(int inventoryNumber) {
+        this.inventoryNumber = inventoryNumber;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocalization() {
+        return localization;
+    }
+
+    public void setLocalization(String localization) {
+        this.localization = localization;
+    }
+
+    public String getPreviousOwner() {
+        return previousOwner;
+    }
+
+    public void setPreviousOwner(String previousOwner) {
+        this.previousOwner = previousOwner;
+    }
+
+    public String getAccessory() {
+        return accessory;
+    }
+
+    public void setAccessory(String accessory) {
+        this.accessory = accessory;
+    }
+
+    public String getInformation() {
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+}
---
+++ src/main/java/pl/cm/Worker.java
@@ -0,0 +1,30 @@
+package pl.cm;
+
+public class Worker
+{
+    private int ID;
+    private String name;
+    private String secondName;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {return name;    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondNameo() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+}
